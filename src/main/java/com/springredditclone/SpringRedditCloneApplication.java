39b4f9c139b351bd7b3242370b1afce418a2e684
dab96 <diegobravo.c96@gmail.com>
2025-08-28 18:16:31 -0500

    backendv1

---
+++ src/main/java/com/springredditclone/SpringRedditCloneApplication.java
@@ -0,0 +1,18 @@
+package com.springredditclone;
+
+import com.springredditclone.config.OpenAPIConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@Import(OpenAPIConfiguration.class)
+public class SpringRedditCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedditCloneApplication.class, args);
+    }
+
+}
